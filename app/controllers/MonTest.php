98b0c13fb996ff3e672ef280ec0155508edcdd03
Thomas Deniaux <tdeniaux@sio.local>
2020-12-03 11:57:01 +0100

    Début MonTest pour QCMController

--- app/controllers/MonTest.php
+++ app/controllers/MonTest.php
@@ -1,6 +1,10 @@
 <?php
 namespace controllers;
 use services\UIService;
+use Ubiquity\utils\http\URequest;
+use models\Qcm;
+use Ubiquity\orm\DAO;
+use models\Question;
 
 /**
  * Controller MonTest
@@ -18,7 +22,35 @@ class MonTest extends ControllerBase{
     }
     
 	public function index(){
-		$this->loadView("MonTest/index.html");
+	    $frm = $this->uiService->qcmForm ();
+	   
+	    $frm->fieldAsSubmit ( 'submit', 'blue', 'MonTest/submit', '#response', [
+	        'ajax' => [
+	            'hasLoader' => 'internal'
+	        ]
+	    ] ); 
+	    $this->jquery->renderView("MonTest/index.html");
+	}
+	
+	public function affichageQuestions(){
+	    
+	    $frm = $this->uiService->qcmForm();
+	    $this->jquery->getOnClick ( '#dropdown-form-typeq-0 .item', 'MonTest/detailsQ', '#response', [
+	        'attr' => 'data-value',
+	        'hasLoader' => false
+	    ] );
+	    $this->jquery->renderView ( "MonTest/qcm.html" );
+	    
+	}
+	public function detailsQ($id) {
+	    $type = DAO::getById ( Question::class, 'id=' . $id );
+	    echo $type->getCaption();
+	}
+	
+	public function submit() {
+	    $qcm = new Qcm();
+	    URequest::setValuesToObject ( $qcm );
+	    DAO::insert ( $qcm );
 	}
 }
 
--- app/services/UIService.php
+++ app/services/UIService.php
@@ -4,6 +4,10 @@ namespace services;
 
 use Ajax\php\ubiquity\JsUtils;
 use models\User;
+use models\Qcm;
+use Ubiquity\orm\DAO;
+use models\Question;
+use Ajax\service\JArray;
 
 class UIService {
 	protected $jquery;
@@ -12,6 +16,51 @@ class UIService {
 		$this->jquery = $jq;
 		$this->semantic = $jq->semantic ();
 	}
+	
+	public function qcmForm() {
+	    $q =  new Qcm();
+	    $frm =$this->jquery->semantic ()->dataForm ( 'form',$q );
+	    $frm->setFields ( [
+	        'QCM Name',
+	        'Description',
+	        'cDate',
+	        'Status',
+	        'Questions',
+	        'submit'
+	    ] );
+	    $frm->fieldAsInput ( 'name', [
+	        'rules' => [
+	            'empty'
+	        ]
+	    ] );
+	    $frm->fieldAsInput ( 'description', [
+	        'rules' => [
+	            'empty'
+	        ]
+	    ] );
+	    $frm->fieldAsInput ( 'cdate', [
+	        'rules' => [
+	            'empty'
+	        ]
+	    ] );
+	    $frm->fieldAsInput ( 'status', [
+	        'inputType' => 'status',
+	        'rules' => [
+	            'length[1]',
+	            'Donnez un status !'
+	        ]
+	    ] );
+	  
+	        $questions = DAO::getAll ( Question::class );
+	        $q->setQuestions( current ( $questions ) );
+	        $frm->fieldAsDropDown ( 'Questions', JArray::modelArray ( $questions, 'getId' ) );
+	        return $frm;
+	        
+	   
+	    
+	    
+	}
+	
 	public function userForm() {
 		$frm = $this->jquery->semantic ()->dataForm ( 'form', new User () );
 		$frm->setFields ( [ 
