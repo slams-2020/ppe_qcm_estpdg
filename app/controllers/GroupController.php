e389671bf77e719295821dd5f79bf87a048cb47d
Sam Potentier <spotentier@sio.local>
2020-12-03 11:19:32 +0100

    creation de groupes

--- app/controllers/GroupController.php
+++ app/controllers/GroupController.php
@@ -1,45 +1,40 @@
 <?php
+
 namespace controllers;
-use controllers\crud\datas\GroupControllerDatas;
-use Ubiquity\controllers\crud\CRUDDatas;
-use controllers\crud\viewers\GroupControllerViewer;
-use Ubiquity\controllers\crud\viewers\ModelViewer;
-use controllers\crud\events\GroupControllerEvents;
-use Ubiquity\controllers\crud\CRUDEvents;
-use controllers\crud\files\GroupControllerFiles;
-use Ubiquity\controllers\crud\CRUDFiles;
 
- /**
-  * CRUD Controller GroupController
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\URequest;
+use models\Group;
+use services\GroupService;
+
+/**
+ * Controller GroupController
+ *
+ * @property \Ajax\php\ubiquity\JsUtils $jquery
  * @route("/Groups","inherited"=>true,"automated"=>true)
-  */
-class GroupController extends \Ubiquity\controllers\crud\CRUDController{
-
-	public function __construct(){
-		parent::__construct();
-		\Ubiquity\orm\DAO::start();
-		$this->model="models\\Group";
+ */
+class GroupController extends ControllerBase {
+	private $GroupService;
+	public function initialize() {
+		parent::initialize ();
+		$this->GroupService = new GroupService ( $this->jquery );
 	}
-
-	public function _getBaseRoute() {
-		return '/Groups';
+	public function index() {
+		$frm = $this->GroupService->userForm ();
+		$frm->fieldAsSubmit ( 'submit', 'blue', 'GroupController/submit', '#response', [ 
+				'ajax' => [ 
+						'hasLoader' => 'internal'
+				]
+		] );
+		$this->jquery->renderView ( "GroupController/index.html" );
+		$groups = DAO::getAll ( Group::class );
+		foreach ( $groups as $group ) {
+			echo $group->getName () . "<br>";
+		}
 	}
-	
-	protected function getAdminData(): CRUDDatas{
-		return new GroupControllerDatas($this);
+	public function submit() {
+		$group = new Group ();
+		URequest::setValuesToObject ( $group );
+		DAO::insert ( $group );
 	}
-
-	protected function getModelViewer(): ModelViewer{
-		return new GroupControllerViewer($this);
-	}
-
-	protected function getEvents(): CRUDEvents{
-		return new GroupControllerEvents($this);
-	}
-
-	protected function getFiles(): CRUDFiles{
-		return new GroupControllerFiles();
-	}
-
-
 }
