5516c835b542224bd9b6756ae8ee2d7bd082c70a
Sam Potentier <spotentier@sio.local>
2020-12-03 12:00:06 +0100

    new style

--- app/cache/annotations/QuestionController.php-75992652.annotations.php
+++ app/cache/annotations/QuestionController.php-75992652.annotations.php
@@ -3,22 +3,16 @@
 return array(
   '#namespace' => 'controllers',
   '#uses' => array (
-  'QuestionControllerDatas' => 'controllers\\crud\\datas\\QuestionControllerDatas',
-  'CRUDDatas' => 'Ubiquity\\controllers\\crud\\CRUDDatas',
-  'QuestionControllerViewer' => 'controllers\\crud\\viewers\\QuestionControllerViewer',
-  'ModelViewer' => 'Ubiquity\\controllers\\crud\\viewers\\ModelViewer',
-  'QuestionControllerEvents' => 'controllers\\crud\\events\\QuestionControllerEvents',
-  'CRUDEvents' => 'Ubiquity\\controllers\\crud\\CRUDEvents',
-  'QuestionControllerFiles' => 'controllers\\crud\\files\\QuestionControllerFiles',
-  'CRUDFiles' => 'Ubiquity\\controllers\\crud\\CRUDFiles',
+  'DAO' => 'Ubiquity\\orm\\DAO',
+  'URequest' => 'Ubiquity\\utils\\http\\URequest',
+  'Question' => 'models\\Question',
+  'Typeq' => 'models\\Typeq',
+  'UIService' => 'services\\UIService',
 ),
   '#traitMethodOverrides' => array (
   'controllers\\QuestionController' => 
   array (
   ),
 ),
-  'controllers\\QuestionController' => array(
-    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "/Questions","inherited"=>true,"automated"=>true)
-  ),
 );
 
