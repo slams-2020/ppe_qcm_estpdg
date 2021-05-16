41c9989dae59abcac028fa35e0019e1e3709a54e
PotentierSam <sam.potentier@sts-sio-caen.info>
2021-05-17 00:22:22 +0200

    eval finished

--- app/controllers/EvalController.php
+++ app/controllers/EvalController.php
@@ -1,15 +1,54 @@
 <?php
 namespace controllers;
 
+use services\EvalService;
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\URequest;
+use models\Exam;
+
 /**
  * Controller EvalController
  *
  * @property \Ajax\php\ubiquity\JsUtils $jquery
  * @route("/Eval","inherited"=>true,"automated"=>true)
  */
-class EvalController
+class EvalController extends ControllerBase
 {
+    private $EvalService;
+    public function initialize() {
+        parent::initialize ();
+        $this->EvalService = new EvalService( $this->jquery );
+    }
+
     public function index() {
+        $frm = $this->EvalService->EvalListe ();
+        $this->jquery->getOnClick('._delete', 'EvalController/suppEval','#main-container',['attr'=>'data-ajax']);
+        $this->jquery->getOnClick('._edit', 'EvalController/afficherEval','#main-container',['attr'=>'data-ajax']);
         $this->jquery->renderView ( "EvalController/index.html" );
+    }
+
+    public function menu() {
+        $frm = $this->EvalService->EvalForm ();
+        $frm->fieldAsSubmit ( 'submit', 'blue', 'EvalController/submit', '#response', [
+            'ajax' => [
+                'hasLoader' => 'internal'
+            ]
+        ] );
+        $this->jquery->renderView ( "EvalController/menu.html" );
+    }
+
+    public function submit() {
+        $Eval = new Exam ();
+        URequest::setValuesToObject ( $Eval );
+        DAO::insert ( $Eval );
+        $frm = $this->EvalService->EvalAjoutForm ( $Eval );
+        $this->jquery->doJQuery ( '#form', 'html', "" );
+        $this->jquery->renderView ( "EvalController/menu.html" );
+    }
+
+    public function suppEval($id){
+        DAO::delete(Exam::class, $id);
+        $frm = $this->EvalService->EvalListe();
+        $this->index();
     }
 }
--- app/services/EvalService.php
+++ app/services/EvalService.php
@@ -5,6 +5,8 @@ namespace services;
 
 
 use Ajax\php\ubiquity\JsUtils;
+use models\Exam;
+use Ubiquity\orm\DAO;
 
 class EvalService
 {
@@ -13,6 +15,51 @@ class EvalService
     public function __construct(JsUtils $jq) {
         $this->jquery = $jq;
         $this->semantic = $jq->semantic ();
+    }
+
+    public function EvalForm() {
+        $frm = $this->jquery->semantic ()->dataForm ( 'form', new Exam() );
+        $frm->setFields ( [
+            'dated',
+            'datef',
+            'submit'
+        ] );
+        $frm->fieldAsInput ( 'dated', [
+            'inputType' => 'datetime-local'
+        ] );
+        $frm->fieldAsInput ( 'datef', [
+            'inputType' => 'datetime-local'
+        ] );
+        return $frm;
+    }
+    public function EvalAjoutForm($Eval) {
+        $dernierExam = DAO::getById ( Exam::class, $Eval->getId() );
+        $frm = $this->jquery->semantic ()->dataElement ( "form", $dernierExam );
+        $frm->setFields ( [
+            'dated',
+            'datef'
+        ] );
+        $frm->setCaptions ( [
+            'Date de Début',
+            'Date de Fin'
+        ] );
+        return $frm;
+    }
+
+    public function EvalListe() {
+        $groups = DAO::getAll ( Exam::class );
+        $table = $this->jquery->semantic ()->dataTable( "tableE", Exam::class,$groups );
+        $table->setFields(['dated','datef',"satus","idGroup"]);
+        $table->setCaptions( [
+            "Date de Début",
+            "Date de Fin",
+            "Status",
+            "Groupe Assigné"
+        ] );
+        $table->setIdentifierFunction('getId');
+        $table->addDeleteButton(false);
+        $table->addEditButton(false);
+        return  $table;
     }
 
 }
